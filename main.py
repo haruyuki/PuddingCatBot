ca4ae715921231e4bb7cdfb1b9da7124e43fa2a5
Oliver Lin <jumpy12359@gmail.com>
2021-06-06 15:51:00 +1000

    Organise imports

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import os
+import logging
 try:
     from dotenv import load_dotenv  # Used for local debugging purposes
     load_dotenv()
 except ModuleNotFoundError:
     pass
 
-import logging
-
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 bot_token = os.getenv('TOKEN')
